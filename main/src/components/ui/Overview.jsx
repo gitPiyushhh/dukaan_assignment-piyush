cf77f167088363e20e712c8902fe44c40e83f0cd
Piyush <Piyushhhagarwal@gmail.com>
2024-01-09 21:54:38 +0530

    Drop down added

--- main/src/components/ui/Overview.jsx
+++ main/src/components/ui/Overview.jsx
@@ -2,38 +2,54 @@ import React from 'react';
 import OverviewCard from './OverviewCard';
 
 const metaData = [
-    {
-        name: 'Next Payout',
-        price: '2,312.23',
-        isOrders: true,
-        orders: '23',
-        isActive: true 
-    },
-    {
-        name: 'Amount Pending',
-        price: '92,312.20',
-        isOrders: true,
-        orders: '13',
-        isActive: false
-    },
-    {
-        name: 'Amount Processed',
-        price: '23,92,312.19',
-        isOrders: false,
-        isActive: false
-    }
-]
+  {
+    name: 'Next Payout',
+    price: '2,312.23',
+    isOrders: true,
+    orders: '23',
+    isActive: true,
+  },
+  {
+    name: 'Amount Pending',
+    price: '92,312.20',
+    isOrders: true,
+    orders: '13',
+    isActive: false,
+  },
+  {
+    name: 'Amount Processed',
+    price: '23,92,312.19',
+    isOrders: false,
+    isActive: false,
+  },
+];
 
 function Overview() {
   return (
-    <div className='w-full text-stone-800 p-8 pt-6'>
-        <span className='font-bold text-[18px]'>Overview</span>
+    <div className="relative w-full p-8 pt-6 text-stone-800">
+      <span className="text-[18px] font-bold">Overview</span>
 
-        <div className='flex w-full justify-between items-start mt-6 pr-4'>
-            {metaData.map(item => <OverviewCard key={item.name} name={item.name} price={item.price} isOrders={item.isOrders} orders={item.orders} isActive={item.isActive}/>)}
-        </div>
+      <div className="mt-6 flex w-full items-start justify-between pr-4">
+        {metaData.map((item) => (
+          <OverviewCard
+            key={item.name}
+            name={item.name}
+            price={item.price}
+            isOrders={item.isOrders}
+            orders={item.orders}
+            isActive={item.isActive}
+          />
+        ))}
+      </div>
+
+      <div className="absolute right-12 top-8 flex items-center justify-between space-x-2 rounded-md border bg-stone-50 px-4 py-2">
+        <span>This month</span>
+        <span className="flex items-center w-3 h-3">
+          <img src="/down.svg" alt="icon" />
+        </span>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Overview
+export default Overview;
