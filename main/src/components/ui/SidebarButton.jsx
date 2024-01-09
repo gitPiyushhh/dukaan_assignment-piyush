21efec47f428041819e26a5ecbed779aebd7135f
Piyush <Piyushhhagarwal@gmail.com>
2024-01-09 23:44:35 +0530

    Minor refactoring

--- main/src/components/ui/SidebarButton.jsx
+++ main/src/components/ui/SidebarButton.jsx
@@ -6,7 +6,7 @@ function SidebarButton({ icon, name, to }) {
     <NavLink activeClassName="bg-stone-400" to={to}>
       <div
         role="button"
-        className="flex gap-8 bg-transparent px-4 py-[0.7rem] text-stone-200"
+        className="flex gap-8 bg-transparent px-4 py-[0.6rem] text-stone-200"
       >
         <img src={`/${icon}.svg`} alt="Icon_img" className="" />
 
